cd8201f4316571b5d543b85a041a5ad5c372325b
aluve-app <anto.blkp@gmail.com>
2026-08-11 12:28:37 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -13,7 +13,7 @@
  * data basi. Yang di-cache cuma "cangkang" aplikasinya.
  * ============================================================ */
 
-const CACHE_NAME = 'svs-shell-v5';
+const CACHE_NAME = 'svs-shell-v6';
 const APP_SHELL_FILES = [
   './',
   './index.html',
